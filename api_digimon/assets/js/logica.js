7fab71300d1b2a5e42a72b1da24fb0a4e4d71f35
Christian <christianrojas.palomino@gmail.com>
2023-04-18 02:44:18 -0400

    Se agregan eventos, y efectos CSS.

--- api_digimon/assets/js/logica.js
+++ api_digimon/assets/js/logica.js
@@ -1,5 +1,23 @@
 const URL_BASE = 'https://digimon-api.vercel.app/api/digimon';
 let contenido;
+let carta;
+let dataNivel;
+let dataImagenes;
+
+function mostrarNivel() {
+    let contenidoNivel = document.getElementById("contenido2");
+    document.getElementById("tabla_principal").style.display = "none";
+    document.getElementById("galeria").style.display = "none";
+    document.getElementById("carta").style.display = "none";
+    document.getElementById("tabla_nivel").style.display = "block";
+    contenidoNivel.innerHTML = "";
+    for (let temp of dataNivel) {
+        contenidoNivel.innerHTML += ` <tr>
+<td class="zoomText">${temp.name}</td>   
+<td>${temp.level}</td>
+</tr> `
+    }
+}
 
 function tabla(datos) {
     contenido.innerHTML = "";
@@ -15,26 +33,75 @@ function tabla(datos) {
 
 }
 
+function tarjeta(data) {
+    carta.innerHTML = "";
+
+    for (let temp of data) {
+        carta.innerHTML += `
+        <div id="tarjSola" class="card mb-3 container" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${temp.img}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">"NOMBRE: ${temp.name}"</h5>
+          <p class="card-text">NIVEL: "${temp.level}"</p>
+         </div>
+      </div>
+    </div>
+  </div>
+     `
+    }
+
+}
+
+function mostrarImagenes() {
+    let img = document.getElementById("galeria");
+    document.getElementById("tabla_principal").style.display = "none";
+    document.getElementById("carta").style.display = "none";
+    document.getElementById("galeria").style.display = "block";
+
+    img.innerHTML = "";
+    for (let temp of dataImagenes) {
+        img.innerHTML += ` 
+         <div id="card" class="card">
+        <img src="${temp.img}" class="card-img-top rotate" alt=" imagen ${temp.name}">
+        <div class="card-body">
+          <h6 class="card-title">${temp.name}</h6>
+          <p class="card-text">${temp.level}</p>
+        </div>
+      </div> `
+    }
+}
+
 function capturaDato() {
     let nombrePersonaje = document.getElementById("dato").value;
     nombrePersonaje = nombrePersonaje.toLowerCase();
+    document.getElementById("tabla_principal").style.display = "none";
+    document.getElementById("galeria").style.display = "none"
+    document.getElementById("tabla_nivel").style.display = "none";;
+    document.getElementById("carta").style.display = "block";
 
     fetch(URL_BASE + '/name/' + nombrePersonaje)
         .then(response => response.json())
         .then(datos => {
             console.log(datos);
-            tabla(datos);
+            tarjeta(datos);
         });
 }
 
 $(document).ready(function () {
     contenido = document.getElementById("contenido");
+    carta = document.getElementById("carta");
 
     fetch(URL_BASE)
         .then(response => response.json())
         .then(datos => {
             console.log(datos);
             tabla(datos);
+            dataImagenes = datos;
+            dataNivel = datos;
         });
 })
 
